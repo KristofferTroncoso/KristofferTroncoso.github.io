768a68c2c906a377ca4730eed84e2657e71edbc0
Kristoffer <troncosokristoffer@gmail.com>
2018-10-04 17:02:39 -0400

    Add technologies to Technologies section in About component

--- src/components/About.js
+++ src/components/About.js
@@ -19,15 +19,15 @@ function About() {
       <div className="Technologies">
         <h2>Technologies</h2>
         <ul>
-          <li>HTML5</li>
-          <li>CSS - Flexbox, Grid, Animations, Frameworks/Bootstrap, SASS, BEM, Responsive, Mobile-first</li>
+          <li>HTML5 - Semantic Elements, Accessibility (WAI-ARIA)</li>
+          <li>CSS - Flexbox, Grid, Animations, Frameworks/Bootstrap, SASS, BEM, Responsive, Mobile-first, CSS Modules</li>
           <li>JavaScript - ES5, ES6/ES2015-2018, AJAX, Fetch API, jQuery, OOP, Functional</li>
-          <li>React - JSX, Redux, React-Router, Animations</li>
+          <li>React - JSX, Redux, React-Router, Animations, CSS Modules, CSS-In-JS (Styled-Components), Testing (Jest w/ Enzyme)</li>
           <li>Node.js - Express, Templating/EJS, NPM, REST</li>
           <li>NoSQL - MongoDB (Mongoose)</li>
           <li>PWAs, SPAs, MPAs, static websites</li>
           <li>Git - Github, GitLab, GitFlow</li>
-          <li>Testing - Jasmine, Jest, Mocha, Chai</li>
+          <li>Testing - unit and integration tests, snapshot testing, Jest, Enzyme, Jasmine</li>
           <li>Cloud/Deployment - AWS, Heroku, Github Pages, Google App Engine, Firebase</li>
         </ul>
       </div>
