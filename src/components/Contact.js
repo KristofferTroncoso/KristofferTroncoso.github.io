f4b3ef2fc0dc420d7ad9d6dbe599cfc0490c7b35
Kristoffer <troncosokristoffer@gmail.com>
2018-10-16 12:46:57 -0400

    Change mail formatting in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     <div className="Contact wrapper">
       <h2>Contact:</h2>
       <ul>
-        <li><strong>Email:</strong>  <span>troncosokristoffer@gmail.com</span></li>
+        <li><strong>&#x2709;  </strong>  <span>troncosokristoffer&#64;gmail.com</span></li>
         <li><strong>Location:</strong>  <span>NYC, New York</span></li>
       </ul>
     </div>
