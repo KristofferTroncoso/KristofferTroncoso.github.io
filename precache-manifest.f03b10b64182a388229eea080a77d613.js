32babc37600e42b4c0340fb738662a61a494051e
Kristoffer <troncosokristoffer@gmail.com>
2019-05-18 18:45:21 -0400

    Updates

---
+++ precache-manifest.f03b10b64182a388229eea080a77d613.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4eef18c81c62a5830842a7ded109c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3a1eb439b2a3288c83",
+    "url": "/static/css/main.32857809.chunk.css"
+  },
+  {
+    "revision": "a19d50b9427725c60a1f",
+    "url": "/static/js/2.dc870e1f.chunk.js"
+  },
+  {
+    "revision": "fa3a1eb439b2a3288c83",
+    "url": "/static/js/main.6865e033.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24bb49bd0b4a1be4458355e390073118",
+    "url": "/static/media/codesandboxlogo.24bb49bd.png"
+  },
+  {
+    "revision": "701e77ef2554f80446357391f09d647d",
+    "url": "/static/media/imessage-thumb.701e77ef.jpeg"
+  },
+  {
+    "revision": "7840e8ecb403184041125d6a0d2d5c02",
+    "url": "/static/media/kht-ui-thumb.7840e8ec.png"
+  },
+  {
+    "revision": "5d15270361b781bc1b483869a4d63e66",
+    "url": "/static/media/kt-portfolio-thumb.5d152703.JPG"
+  },
+  {
+    "revision": "0062f07b18334f681a9e29e85af0b8c9",
+    "url": "/static/media/lol-api-thumb.0062f07b.jpg"
+  },
+  {
+    "revision": "3ce8637b59f0253c6c60a4705d817da4",
+    "url": "/static/media/marvel-thumb.3ce8637b.jpg"
+  },
+  {
+    "revision": "4b7c318cbd054bbd7cfc6041f2f0acf7",
+    "url": "/static/media/random-user-redux-thumb.4b7c318c.jpg"
+  },
+  {
+    "revision": "667459ddfc49066d717837b461a016c2",
+    "url": "/static/media/react-easy-code-thumb.667459dd.JPG"
+  },
+  {
+    "revision": "4824e2d746619174ddce4af3211be784",
+    "url": "/static/media/react-image-editor-thumb.4824e2d7.jpg"
+  },
+  {
+    "revision": "1fde7be560cad55121b4a67677384a2e",
+    "url": "/static/media/rheastagram-thumb.1fde7be5.jpeg"
+  }
+]);
